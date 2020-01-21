b5a732558732a64cc9cdd0f1f913260f9ab59bcc
alexander215 <alexander.hoskinson@colorado.edu>
2020-01-20 20:59:11 -0800

    Example of React.createElement instead of JSX code

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ class App extends Component {
         <h1>Hi, I'm a React App</h1>
       </div>
     );
+
+    // Alone, this code would render the message, but it considers h1 as text:
+    // return React.createElement('div', null, 'h1', 'Hi, I\'m a React App!!!')
+    
+    // Nested create element is the same as the above JSX code:
+    // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, null, 'Does this work now?'))
+    // It's why we import React even if it doesn't appear to be used.
+    // It is used when compiling
   }
 }
 
