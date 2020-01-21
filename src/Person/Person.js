c467e8949cfcf4ccb502ff5955ce587f49b38d7b
alexander215 <alexander.hoskinson@colorado.edu>
2020-01-20 21:27:04 -0800

    Pass children to Person component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,7 +4,12 @@ const person = (props) => {
     // Simple, undynamic way of doing things:
     // return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!!</p>
     
-    return <p>I'm {props.name} and I am {props.age} years old!!</p>
+    return (
+        <div>
+            <p>I'm {props.name} and I am {props.age} years old!!</p>
+            <p>{props.children}</p>
+        </div>
+    )
 };
 
 export default person;
