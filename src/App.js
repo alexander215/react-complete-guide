5b7b5854cf5304065c2bd8b04920d8320fb4518f
alexander215 <alexander.hoskinson@colorado.edu>
2020-01-21 18:11:37 -0800

    Bind and anonymous function for onClick in app.js

--- src/App.js
+++ src/App.js
@@ -1,57 +1,49 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////// SAME COMPONENT BUT USING HOOKS
+//////////// SAME COMPONENT BUT USING CLASS
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// import React, { Component } from 'react';
-import React, { useState } from 'react';
+
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-  const [ personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 }
-    ]
-  });
-  
-  const [otherState, setOtherState] = useState('Some other value')
-  console.log(personsState, otherState)
+    ],
+    otherState: 'some other value'
+  }
 
-  const switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked')
     // DON'T DO THIS: this.state.persons[0].name = "Maximillian";
-    setPersonsState({
+    this.setState({
       persons: [
-        { name: 'Maximilian', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 27 }
       ]
-    });
-  };
+    })
+  }
 
-
-  // render() {
+  render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
         <Person 
-          name={personsState.persons[0].name} 
-          age={personsState.persons[0].age} 
-        />
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
         <Person 
-          name={personsState.persons[1].name} 
-          age={personsState.persons[1].age} >
-          My hobbies: Racing
-        </Person>
-        <Person 
-          name={personsState.persons[2].name} 
-          age={personsState.persons[2].age}
-        />
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')} >My hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
 
@@ -63,75 +55,64 @@ const app = props => {
     // It's why we import React even if it doesn't appear to be used.
     // It is used when compiling
   }
-// }
+}
 
-export default app;
-
-
-// Removed from class-based component. 
-// Save for after using hooks.
-// state = {
-//   persons: [
-//     { name: 'Max', age: 28 },
-//     { name: 'Manu', age: 29 },
-//     { name: 'Stephanie', age: 26 }
-//   ],
-//   otherState: 'some other value'
-// }
-
-// switchNameHandler = () => {
-//   // console.log('Was clicked')
-//   // DON'T DO THIS: this.state.persons[0].name = "Maximillian";
-//   this.setState({
-//     persons: [
-//       { name: 'Maximilian', age: 28 },
-//       { name: 'Manu', age: 29 },
-//       { name: 'Stephanie', age: 27 }
-//     ]
-//   })
-// }
+export default App;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////// SAME COMPONENT BUT USING CLASS
+//////////// SAME COMPONENT BUT USING HOOKS
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// import React, { Component } from 'react';
+// // import React, { Component } from 'react';
+// import React, { useState } from 'react';
 // import './App.css';
 // import Person from './Person/Person';
 
-// class App extends Component {
-//   state = {
+// const app = props => {
+//   const [ personsState, setPersonsState] = useState({
 //     persons: [
 //       { name: 'Max', age: 28 },
 //       { name: 'Manu', age: 29 },
 //       { name: 'Stephanie', age: 26 }
-//     ],
-//     otherState: 'some other value'
-//   }
+//     ]
+//   });
+  
+//   const [otherState, setOtherState] = useState('Some other value')
+//   console.log(personsState, otherState)
 
-//   switchNameHandler = () => {
+//   const switchNameHandler = () => {
 //     // console.log('Was clicked')
 //     // DON'T DO THIS: this.state.persons[0].name = "Maximillian";
-//     this.setState({
+//     setPersonsState({
 //       persons: [
 //         { name: 'Maximilian', age: 28 },
 //         { name: 'Manu', age: 29 },
 //         { name: 'Stephanie', age: 27 }
 //       ]
-//     })
-//   }
+//     });
+//   };
 
-//   render() {
+
+//   // render() {
 //     return (
 //       <div className="App">
 //         <h1>Hi, I'm a React App</h1>
 //         <p>This is really working!</p>
-//         <button onClick={this.switchNameHandler}>Switch Name</button>
-//         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-//         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My hobbies: Racing</Person>
-//         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+//         <button onClick={switchNameHandler}>Switch Name</button>
+//         <Person 
+//           name={personsState.persons[0].name} 
+//           age={personsState.persons[0].age} 
+//         />
+//         <Person 
+//           name={personsState.persons[1].name} 
+//           age={personsState.persons[1].age} >
+//           My hobbies: Racing
+//         </Person>
+//         <Person 
+//           name={personsState.persons[2].name} 
+//           age={personsState.persons[2].age}
+//         />
 //       </div>
 //     );
 
@@ -143,6 +124,30 @@ export default app;
 //     // It's why we import React even if it doesn't appear to be used.
 //     // It is used when compiling
 //   }
-// }
+// // }
 
-// export default App;
+// export default app;
+
+
+// // Removed from class-based component. 
+// // Save for after using hooks.
+// // state = {
+// //   persons: [
+// //     { name: 'Max', age: 28 },
+// //     { name: 'Manu', age: 29 },
+// //     { name: 'Stephanie', age: 26 }
+// //   ],
+// //   otherState: 'some other value'
+// // }
+
+// // switchNameHandler = () => {
+// //   // console.log('Was clicked')
+// //   // DON'T DO THIS: this.state.persons[0].name = "Maximillian";
+// //   this.setState({
+// //     persons: [
+// //       { name: 'Maximilian', age: 28 },
+// //       { name: 'Manu', age: 29 },
+// //       { name: 'Stephanie', age: 27 }
+// //     ]
+// //   })
+// // }
