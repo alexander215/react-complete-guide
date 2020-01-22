66254b44d44c1a47d8d74ceab87acddc0776c8c1
alexander215 <alexander.hoskinson@colorado.edu>
2020-01-22 13:16:25 -0800

    Add key to Person component list items

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: 'ahrhwhdg', name: 'Max', age: 28 },
+      { id: 'jetyjekg', name: 'Manu', age: 29 },
+      { id: 'fhfdshhw', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
@@ -74,7 +74,8 @@ class App extends Component {
             return <Person
               click={() => this.deletePersonHandler(index)} 
               name={person.name} 
-              age={person.age} />
+              age={person.age}
+              key={person.id} />
           })}
           {/* The list before mapping: */}
           {/* <Person 
