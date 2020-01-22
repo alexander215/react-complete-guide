f6d0598167381168e7575a8b197ca7dd5c220255
alexander215 <alexander.hoskinson@colorado.edu>
2020-01-22 11:59:10 -0800

    Conditional variable inside render to show hide info onClick

--- src/App.js
+++ src/App.js
@@ -55,6 +55,24 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if (this.state.showPersons){
+      persons = (
+        <div>
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age} />
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Max!')} 
+            changed={this.nameChangedHandler}>My hobbies: Racing</Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
@@ -64,19 +82,7 @@ class App extends Component {
           // This is the sytnax to use a function and pass an argument
           // onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        { this.state.showPersons ? 
-            <div>
-              <Person 
-                name={this.state.persons[0].name} 
-                age={this.state.persons[0].age} />
-              <Person 
-                name={this.state.persons[1].name} 
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, 'Max!')} 
-                changed={this.nameChangedHandler}>My hobbies: Racing</Person>
-              <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-            </div> : null
-        }
+          {persons}
       </div>
     );
 
