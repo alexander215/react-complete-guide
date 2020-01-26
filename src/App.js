8bec6a96d91defa0d89264594cf0f328c2ae7ecd
alexander215 <alexander.hoskinson@colorado.edu>
2020-01-25 16:18:02 -0800

    Import Styled Components to make StyleDiv in Person.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium, { StyleRoot } from 'radium';
+// import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
   state = {
@@ -125,7 +125,7 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
+      // <StyleRoot> // This is for Radium
         <div className="App">
           <h1>Hi, I'm a React App</h1>
           <p className={classes.join(' ')}>This is really working!</p>
@@ -136,7 +136,7 @@ class App extends Component {
             onClick={this.togglePersonsHandler}>Toggle Persons</button>
             {persons}
         </div>
-      </StyleRoot>
+      // </StyleRoot>
     );
 
     // Alone, this code would render the message, but it considers h1 as text:
@@ -149,7 +149,8 @@ class App extends Component {
   }
 }
 
-export default Radium(App);
+export default App;
+// export default Radium(App);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
