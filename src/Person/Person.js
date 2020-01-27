5660e5303f5bca0f9b24672090e07efd5198d207
alexander215 <alexander.hoskinson@colorado.edu>
2020-01-26 22:07:56 -0800

    Add CSS modules to Person component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import './Person.css';
 // import Radium from 'radium';
-import styled from 'styled-components';
+// import styled from 'styled-components';
+import classes from './Person.css';
 
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
+// const StyledDiv = styled.div`
+//     width: 60%;
+//     margin: 16px auto;
+//     border: 1px solid #eee;
+//     box-shadow: 0 2px 3px #ccc;
+//     padding: 16px;
+//     text-align: center;
 
-    @media (min-width: 500px){
+//     @media (min-width: 500px){
     
-        width: 450px;
-    }
-`;
+//         width: 450px;
+//     }
+// `;
 
 const person = (props) => {
     // Simple, undynamic way of doing things:
@@ -27,13 +28,15 @@ const person = (props) => {
     };
     return (
         // <div className="Person" style={style}>
-        <StyledDiv>
+        // <StyledDiv>
+        <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
             {/* Below is the less dynamic way to do this before having a key */}
             {/* <input type="text" onChange={props.changed} value={props.name} /> */}
-        </StyledDiv>
+        </div>
+        // </StyledDiv>
         // </div>
     )
 };
