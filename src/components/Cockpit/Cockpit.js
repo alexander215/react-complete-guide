b8313300df277dddad0a73f70d70cc098a5a90c8
alexander215 <alexander.hoskinson@colorado.edu>
2020-04-27 23:06:47 -0700

    clearTimeout for the timer

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -5,10 +5,11 @@ const cockpit = (props) => {
     useEffect(() => {
       console.log('[Cockpit.js] useEffect')
       // Http request...
-      setTimeout(() => {
+      const timer = setTimeout(() => {
         alert('Saved data to cloud!')
       }, 1000)
       return () => {
+        clearTimeout(timer);
         console.log('[Cockpit.js] cleanup work in useEffect');
       }
       // Adding additional parameters causes this only to run when those variables changes:
